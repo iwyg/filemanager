7fa1bf68adaf5c7d2607aa0f5802a16216282225
iwyg <mail@thomas-appel.com>
2012-06-04 12:50:17 +0200

    last build got corrupted

--- assets/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-/**
- * @package filemanager
- * @author thomas appel <mail@thomas-appel.com>
-
- * Displays <a href="http://opensource.org/licenses/gpl-3.0.html">GNU Public License</a>
- * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
- */
-
-(function(a){a("bootstrap",["fm_settings"],function(a){return function(b,c){a.instances["filemanager-"+c]={field_id:b,instance:c}}})})(this.define)
--- assets/js/modules/mod_byteconverter.js
+++
@@ -1,9 +0,0 @@
-/**
- * @package Modules
- * @author thomas appel <mail@thomas-appel.com>
-
- * Displays <a href="http://opensource.org/licenses/gpl-3.0.html">GNU Public License</a>
- * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
- */
-
-(function(a){var b=1e3,c=Math.pow(b,2),d=Math.pow(b,3);a([],function(){function a(a,b){return(Math.round(a/b*1e5)/1e5).toFixed(2)}function e(c){return a(c,b)}function f(b){return a(b,c)}function g(b){return a(b,d)}function h(a){return a<b?a+"":a<c?e(a)+" KB":a<d?f(a)+" MB":g(a)+" GB"}return h})})(this.define)
