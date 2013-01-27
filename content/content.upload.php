f9dabe952a340cdbb4bcaad30b285081ce4cbe8a
iwyg <mail@thomas-appel.com>
2013-01-28 00:01:44 +0100

    bug fix

--- content/content.upload.php
+++ content/content.upload.php
@@ -79,7 +79,7 @@ class contentExtensionFilemanagerUpload extends contentExtensionFilemanagerSetti
 
 		$unique = intval($this->get('unique_file_name'), 10) == 1;
 
-        $dest = DOCROOT . $this->get('destination') . DIRECTORY_SEPARATOR . $this->sanitizePathFragment($data['destination']) . DIRECTORY_SEPARATOR;
+        $dest = WORKSPACE . DIRECTORY_SEPARATOR . $this->sanitizePathFragment($data['destination']) . DIRECTORY_SEPARATOR;
 
 		$results = array();
 
